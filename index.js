7d955d4624e34cbc00854f1b64c9adc82256c135
Monjure Alahi Saiyan <sayemmetro420@gmail.com>
2026-02-18 23:01:19 +0600

    Final Update to clean finish

--- index.js
+++ index.js
@@ -1,65 +1,28 @@
 let allProducts = [];
-const loadAllProducts = () =>{
-    fetch("https://fakestoreapi.com/products")
+const loadAllProducts = async () =>{
+     await fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
     .then((data) => {
-        allProducts = data;   
-        displayAllProducts(data);
+      
+        allProducts = data; 
+        topRatedProducts(data); 
+        displayAllProducts(data); 
+                
+        console.log("All products loaded", displayAllProducts)
+        
     })
 }
-const loadAllProducts2 = () =>{
-    fetch("https://fakestoreapi.com/products")
+const loadAllCategories = async () =>{
+    await fetch("https://fakestoreapi.com/products/categories")
     .then((res) => res.json())
     .then((data) => {
-        trendyProducts(data);
-    })
+      data.unshift('All')
+      displayCategory(data)})
 }
-const loadAllCategories = () =>{
-    fetch("https://fakestoreapi.com/products/categories")
-    .then((res) => res.json())
-    .then((data) => displayCategory(data))
-}
-
-const filterByCategory = (categoryName) => {
-
-    if(categoryName === "all"){
-        displayAllProducts(allProducts);
-        return;
-    }
-
-    const filteredProducts = allProducts.filter(product =>
-        product.category === categoryName
-    );
-
-    displayAllProducts(filteredProducts);
-};
-
-const displayCategory = (categories)=>{
-console.log(categories)
-loadAllProducts();
-const allProductContainer = document.getElementById("service-cart-container")
-    
-const allCategories = document.getElementById("categories")
-    allProductContainer.innerHTML = ""
-    allCategories.innerHTML = ""
-    allCategories.innerHTML = `<button onClick="loadAllProducts()"  class="btn border-1 border-[#3B25C1] hover:border-[#3B25C1] bg-[#FFFFFF] hover:bg-[#3B25C1] focus:bg-[#3B25C1] text-[#3B25C1] hover:text-[#FFFFFF] focus:text-[#FFFFFF]   rounded-full">  All</button>`
-
-    for(let category of categories){
-        const categoryDiv = document.createElement("button")
-        categoryDiv.classList.add("btn", "border-1" ,"border-[#3B25C1]", "hover:border-[#3B25C1]", "bg-[#FFFFFF]", "hover:bg-[#3B25C1]", "focus:bg-[#3B25C1]", "text-[#3B25C1]", "hover:text-[#FFFFFF]", "focus:text-[#FFFFFF]" ,  "rounded-full");
-        categoryDiv.innerHTML=`
-        <button  > ${category}  </button>     `
-        categoryDiv.onclick = () => {
-            filterByCategory(category);   // ✅ Pass category name
-        };
-        allCategories.append(categoryDiv)
-    }
-
-};
 const displayAllProducts = (products)=>{
-console.log(products)
 
-const allProductContainer = document.getElementById("service-cart-container")
+console.log(products)
+const allProductContainer = document.getElementById("product-container")
     allProductContainer.innerHTML = ""
 
     for(let product of products){
@@ -69,7 +32,7 @@ const allProductContainer = document.getElementById("service-cart-container")
         productDiv.innerHTML=`
         
           <figure>
-            <img class="max-h-[300px]" src="${product.image}" alt="Shoes" />
+            <img class="max-h-[300px] min-h-[300px]" src="${product.image}" alt="Shoes" />
           </figure>
           <div class="card-body">
             <div class="flex justify-between items-center">
@@ -81,9 +44,9 @@ const allProductContainer = document.getElementById("service-cart-container")
             <h2 class="card-title -mb-2 mt-4 truncate text-overflow: ellipsis;" >
               ${product.title}
             </h2>
-            <p class="font-bold text-[15px]" >$ ${product.price}</p>
+            <p class="font-bold text-[15px]" >$${product.price}</p>
             <div class="card-actions justify-between mt-5">
-              <button class="btn border-1 border-[#DBDEE4] hover:border-[#3B25C1] bg-[#FFFFFF] text-[#5B6473] hover:text-[#3B25C1] lg:w-34 w-28"> <i class="fa-regular fa-eye"></i> Default</button>
+              <button onclick="loadDetails(${product.id})" class="btn border-1 border-[#DBDEE4] hover:border-[#3B25C1] bg-[#FFFFFF] text-[#5B6473] hover:text-[#3B25C1] lg:w-34 w-28"> <i class="fa-regular fa-eye"></i> Default</button>
               <button class="btn border-1 border-[#3B25C1] hover:border-[#3B25C1] bg-[#3B25C1] hover:bg-[#FFFFFF] text-white hover:text-[#3B25C1] lg:w-34 w-28 "> <i class="fa-solid fa-cart-plus"></i> Add</button>
             </div>
           </div>
@@ -93,21 +56,57 @@ const allProductContainer = document.getElementById("service-cart-container")
     }
 
 };
-const trendyProducts = (products)=>{
-console.log(products)
 
-const allProductContainer = document.getElementById("top-product-container")
-    allProductContainer.innerHTML = ""
-    const mostRatedProduct = products.filter(product =>(product?.rating?.count) === 300)
-    console.log("most rated", mostRatedProduct)
-    for(let product of mostRatedProduct){
+const filterByCategory = (categoryName) => {
+    if(categoryName === "All"){
+      console.log("Get all products")
+        displayAllProducts(allProducts);
+        return;
+    }
+    const filteredProducts = allProducts.filter(product =>
+        product.category === categoryName
+    );
+    console.log("Get Category products")
 
+    displayAllProducts(filteredProducts);
+};
+const topRatedProducts = (products) =>{
+  // console.log("Get top rated products")
+    const mostRatedProduct = products.filter(product =>(product?.rating?.count) > 450)
+    console.log(mostRatedProduct);
+    displayTopProducts(mostRatedProduct);
+   
+}
+const displayCategory = (categories)=>{
+// console.log(categories)
+const allCategories = document.getElementById("categories")
+    allCategories.innerHTML = ""
+    for(let category of categories){
+        const categoryDiv = document.createElement("button")
+        categoryDiv.classList.add("btn", "border-1" ,"border-[#3B25C1]", "hover:border-[#3B25C1]", "bg-[#FFFFFF]", "hover:bg-[#3B25C1]", "focus:bg-[#3B25C1]","focus:text-[#FFFFFF]", "text-[#3B25C1]", "hover:text-[#FFFFFF]", "focus:text-[#FFFFFF]" ,"m-2",  "rounded-full");
+        categoryDiv.innerHTML=`${category}`
+        categoryDiv.onclick = () => {
+            filterByCategory(category);   
+        };
+        allCategories.append(categoryDiv)
+    }
+
+};
+
+const displayTopProducts = (products)=>{
+// console.log(products)
+
+ const topProductContainer = document.getElementById("top-product-container")
+    // topProductContainer.innerHTML = ""
+
+    for(let product of products){
+       
         const productDiv = document.createElement("div")
-        productDiv.classList.add("card" ,"bg-base-100", "w-84", "shadow-sm");
+        productDiv.classList.add("card" ,"bg-base-100", "lg:w-84", "shadow-sm");
         productDiv.innerHTML=`
         
           <figure>
-            <img class="max-h-[300px]" src="${product.image}" alt="Shoes" />
+            <img class="max-h-[300px] min-h-[300px] "  src="${product.image}" alt="Shoes" />
           </figure>
           <div class="card-body">
             <div class="flex justify-between items-center">
@@ -119,23 +118,55 @@ const allProductContainer = document.getElementById("top-product-container")
             <h2 class="card-title -mb-2 mt-4 truncate text-overflow: ellipsis;" >
               ${product.title}
             </h2>
-            <p class="font-bold text-[15px]" >$ ${product.price}</p>
+            <p class="font-bold text-[15px]" >$${product.price}</p>
             <div class="card-actions justify-between mt-5">
-              <button class="btn border-1 border-[#DBDEE4] hover:border-[#3B25C1] bg-[#FFFFFF] text-[#5B6473] hover:text-[#3B25C1] lg:w-34 w-28"> <i class="fa-regular fa-eye"></i> Default</button>
+              <button onclick="loadDetails(${product.id})" class="btn border-1 border-[#DBDEE4] hover:border-[#3B25C1] bg-[#FFFFFF] text-[#5B6473] hover:text-[#3B25C1] lg:w-34 w-28"> <i class="fa-regular fa-eye"></i> Default</button>
               <button class="btn border-1 border-[#3B25C1] hover:border-[#3B25C1] bg-[#3B25C1] hover:bg-[#FFFFFF] text-white hover:text-[#3B25C1] lg:w-34 w-28 "> <i class="fa-solid fa-cart-plus"></i> Add</button>
             </div>
           </div>
         
         `
-        allProductContainer.append(productDiv)
+        topProductContainer.append(productDiv)
     }
-
 
 };
 
+const loadDetails = async (id)=>{
+  console.log(id)
+   await fetch(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+        displayProductDetails(data); 
+        console.log(data); 
+        
+    })
+}
+
+const displayProductDetails = (product) =>{
+const detailsContainer = document.getElementById("details-container")
+      detailsContainer.innerHTML = ` <div class="card-body">
+            <div class="flex justify-between items-center">
+              <div class=""><p class="font-bold text-[15px]" >$${product.price}</p></div>
+              <div class=""> <i class="fa-solid fa-star text-[#FED232]"></i>
+                <span class="text-[#5B6473]">${product?.rating?.rate} (${product?.rating?.count})</span>
+              </div>
+            </div>
+            <h2 class="card-title -mb-2 py-4 " >
+              ${product.title}
+            </h2>
+            <p class="text-[13px] mb-4" >$${product.description}</p>
+            
+            <div class="card-actions justify-between mt-5">
+              <button " class="btn border-1 border-[#DBDEE4] hover:border-[#3B25C1] bg-[#FFFFFF] text-[#5B6473] hover:text-[#3B25C1] ">Buy Now</button>
+              <button class="btn border-1 border-[#3B25C1] hover:border-[#3B25C1] bg-[#3B25C1] hover:bg-[#FFFFFF] text-white hover:text-[#3B25C1]  "> <i class="fa-solid fa-cart-plus"></i> Add to Cart</button>
+            </div>
+          </div>`
+
+console.log(product)
+document.getElementById("my_modal_5").showModal()
+}
 
 
-
-loadAllProducts2();
 loadAllProducts();
 loadAllCategories();
+
